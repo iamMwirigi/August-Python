1f94f33c7d4f29bec4b27529973a129e123df6d9
Evans-mutuku <evansnyamai98@gmail.com>
2024-11-19 20:26:52 +0300

    adding the front end

---
+++ WEEK 4/exception.py
@@ -0,0 +1,16 @@
+a = 5
+b = 0
+
+# print(a/b)
+
+try:
+    print(a/b)
+    k = int(input("Enter a number: "))
+except Exception as e:
+    print("You can't divide by zero", e)
+except ZeroDivisionError as e:
+    print("You can't divide by zero", e)
+except ValueError as e:
+    print("You can't divide by zero", e)
+finally:
+    print("You gat this bro")
